8b5adc0e27573a599f73645399f1cdb7c13ef3bf
renosyah <syahputrareno975@gmail.com>
2022-10-07 13:16:55 +0700

    improve animation on jump

--- entity/unit/ground-unit/fox/fox.gd
+++ entity/unit/ground-unit/fox/fox.gd
@@ -4,6 +4,7 @@ onready var animation_state = $pivot/AnimationTree.get("parameters/playback")
 onready var firing_delay = $firing_delay
 
 var node_not_move = ["Attack"]
+var is_jump = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,7 +14,7 @@ remotesync func _attack():
 	animation_state.travel("Attack")
 	
 remotesync func _jump():
-	animation_state.travel("ToucheGround")
+	animation_state.travel("Jump")
 	
 func attack():
 	#.attack()
@@ -29,13 +30,24 @@ func jump():
 	if not _is_master():
 		return
 		
-	if is_on_floor():
+	if is_on_floor() and not is_jump:
+		is_jump = true
 		_snap = Vector3.ZERO
 		translation.y += 0.55
-		_velocity.y = 35.0
+		_velocity.y = 15.0
 		rpc_unreliable("_jump")
 	
+func master_moving(delta):
+	.master_moving(delta)
+	
+	if is_jump and is_on_floor():
+		is_jump = false
+		animation_state.travel("ToucheGround")
+	
 func _on_animation_checker_timeout():
+	if is_jump:
+		return
+		
 	if move_direction != Vector2.ZERO:
 		animation_state.travel("Run")
 	else:
